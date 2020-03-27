353698a24b1343f1018444f3fbbebd4a68285de1
姚非凡 <7370372+yao_extraordinary@user.noreply.gitee.com>
2020-03-27 20:14:37 +0800

    完成了登录功能

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,8 @@ module.exports = {
     parser: 'babel-eslint'
   },
   rules: {
+    "space-before-function-paren": 0,
+    "indent": ["off", 2],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
